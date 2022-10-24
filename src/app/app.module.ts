88bde631e0f3ecb6938bd60cef65d3362ed83c86
Alfredo Duenas <alfredoduenasjr1982@gmail.com>
2022-10-24 12:08:31 -0400

    step 1 commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MastheadComponent } from './components/masthead/masthead.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MastheadComponent
   ],
   imports: [
     BrowserModule,
