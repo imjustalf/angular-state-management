379876ba5addaf4b42ea7decb5981b94f2d68879
Alfredo Duenas <alfredoduenasjr1982@gmail.com>
2022-10-25 15:18:25 -0400

    after break tuesday

--- src/app/features/redux-counter/state/reducers/counter.reducer.ts
+++ src/app/features/redux-counter/state/reducers/counter.reducer.ts
@@ -4,26 +4,32 @@ import { CounterComponentEvents } from '../actions/counter.actions';
 // describe state for typescript
 export interface CounterState {
   current: number;
+  by: 1 | 3 | 5;
 }
 
 // what should inital state of this be when the application starts up.
 
 const initialState: CounterState = {
   current: 0,
+  by: 1,
 };
 
 export const reducer = createReducer(
   initialState,
+  on(CounterComponentEvents.by, (currentState, a) => ({
+    ...currentState,
+    by: a.by,
+  })),
   on(
     CounterComponentEvents.incremented,
     (currentState: CounterState): CounterState => ({
       ...currentState,
-      current: currentState.current + 1,
+      current: currentState.current + currentState.by,
     }),
   ),
-  on(CounterComponentEvents.decremented, (s) => ({
-    ...s,
-    current: s.current - 1,
+  on(CounterComponentEvents.decremented, (currentState) => ({
+    ...currentState,
+    current: currentState.current - currentState.by,
   })),
   on(CounterComponentEvents.reset, (s) => initialState),
 );
